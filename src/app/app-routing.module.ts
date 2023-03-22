e467a6855c3082764cefb0d2225f253da6ecdd84
Edgarrhz03 <al222010206@gmail.com>
2023-03-21 19:29:28 -0600

    profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComoponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: "profile",
+  component: ProfileComoponent
+  
+}
+
+];
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComoponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+
+    AppComponent,
+    ProfileComoponent
   ],
   imports: [
     BrowserModule,
