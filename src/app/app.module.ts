5a9c0d22a0b798cb9db208ed3c0820c49942fe8c
Iván Villalóz Piña <80796529+IvanVillalozPina@users.noreply.github.com>
2023-03-21 19:33:22 -0600

    xd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+
 import { ProfileComoponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-
     AppComponent,
     ProfileComoponent
-=======
-//IMPORTS
-import { LoginComponent } from './components/auth/login/login.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
->>>>>>> 9d54c9a419b12e63e9496fb84dbd324c228e1e7c
   ],
   imports: [
     BrowserModule,
