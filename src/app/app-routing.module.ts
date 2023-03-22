edcf43849e043a49c64e104c731f5dfcc4b55b6e
Edgarrhz03 <al222010206@gmail.com>
2023-03-21 19:38:06 -0600

    profile01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
+import { LoginComponent } from './components/auth/login/login.component';
 import { ProfileComoponent } from './components/profile/profile.component';
+
 
 const routes: Routes = [
 {
   path: "profile",
   component: ProfileComoponent
   
+},
+{
+  path: "login",
+  component: LoginComponent
 }
 
 ];
 
 
 
-=======
-//IMPORTS
-import { LoginComponent } from './components/auth/login/login.component';
-
-const routes: Routes = [
-  {
-    path: "login",
-    component: LoginComponent
-  },
-];
->>>>>>> 9d54c9a419b12e63e9496fb84dbd324c228e1e7c
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
