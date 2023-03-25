3c1a80edf6c73d0c15b450ba0e3fea564f15e746
dyep.frontend <rdylan3871@gmail.com>
2023-03-25 13:58:54 -0600

    Registro completo solo falta scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { ProfileComoponent } from './components/profile/profile.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComoponent,
-    LoginComponent
+    LoginComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss'],
-  
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-
+  constructor(private http: HttpClient) {}
+  
+  onSubmit(event: Event) {
+    event.preventDefault();
+    const target = event.target as HTMLFormElement;
+    const name = (target.querySelector('#name') as HTMLInputElement).value;
+    const email = (target.querySelector('#email') as HTMLInputElement).value;
+    const password = (target.querySelector('#password') as HTMLInputElement).value;
+    const confirmPassword = (target.querySelector('#confirmPassword') as HTMLInputElement).value;
+  
+    // Realiza la validación de los campos del formulario
+    if (!name || !email || !password || !confirmPassword) {
+      // Muestra un mensaje de error si algún campo está vacío
+      alert('Todos los campos son obligatorios');
+      return;
+    }
+    if (password !== confirmPassword) {
+      // Muestra un mensaje de error si las contraseñas no coinciden
+      alert('Las contraseñas no coinciden');
+      return;
+    }
+  
+    // Envía los datos al servidor
+    const url = 'https://ejemplo.com/api/register';//aqui va la api
+    const data = { name, email, password };
+    this.http.post(url, data).subscribe(
+      (response) => {
+        // Muestra un mensaje de éxito si la solicitud se realizó correctamente
+        alert('¡Registro exitoso!');
+      },
+      (error) => {
+        // Muestra un mensaje de error si la solicitud falló
+        alert('Error al registrar');
+      }
+    );
+  }
 }
