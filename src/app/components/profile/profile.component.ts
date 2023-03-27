b7c4a3e4bd495c3dbda33dd4573acc3d374275d8
Edgarrhz03 <al222010206@gmail.com>
2023-03-27 13:35:24 -0600

    profile termiadobeta12

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComoponent {
-    name = 'Andres Manuel Lopez Obrador';
-    
+export class ProfileComoponent implements OnInit {
+  name = 'Andres Manuel Lopez Obrador';
+  isLoading = false;
+
+  constructor(private http: HttpClient) { }
+
+  async ngOnInit() {
+    this.isLoading = true;
+    try {
+      const token = sessionStorage.getItem('token');
+      console.log(token)
+      const headers = new HttpHeaders({
+        'Authorization': 'Bearer ' + token,
+        'Content-Type' : 'application/json'
+      });
+
+      const res = await this.http.post<any>('http://127.0.0.1:8080/api/auth/user', {}, { headers }).toPromise();
+      this.name = res.username;
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.isLoading = false;
+    }
+  }
+}
   
     
-  }
+  
   
