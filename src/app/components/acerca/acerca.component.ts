3e4f089da738cc312f9fcbffb0141cc436408b91
CryoXen <al222011226@gmail.com>
2023-03-27 03:10:44 -0600

    acerca de

--- src/app/components/acerca/acerca.component.ts
+++ src/app/components/acerca/acerca.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-acerca',
   templateUrl: './acerca.component.html',
   styleUrls: ['./acerca.component.scss']
+  
 })
 export class AcercaComponent {
+  
 
 }
