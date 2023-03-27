493cdc5ec0c091c19fa041e2f98c5acd186bbe3a
leonellechuga <84852189+Knnivol@users.noreply.github.com>
2023-03-27 09:53:05 -0600

    Publicacioens con string-fecha y comentarios

--- src/app/components/publicaciones/publicaciones.component.ts
+++ src/app/components/publicaciones/publicaciones.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-publicaciones',
@@ -7,6 +9,10 @@ import { Component } from '@angular/core';
 })
 
 export class PublicacionesComponent {
+
+  fecha = new Date();
+
+  fechaString = format(this.fecha, 'dd/MM/yyyy');
 
   openModal() {
     const modal = document.querySelector('.modal');
